fd1b683c9b84c38ce1c7f4a5a8bd8df792733dc8
ravipilgar <65125744+ravipilgar@users.noreply.github.com>
2021-10-29 18:35:29 +0530

    updated Map

--- src/main/java/com/springboot/rest/security/DomainUserDetailsService.java
+++ src/main/java/com/springboot/rest/security/DomainUserDetailsService.java
@@ -39,8 +39,10 @@ public class DomainUserDetailsService implements UserDetailsService {
 
         if (new EmailValidator().isValid(login, null)) {
             
-            userPersistencPort
-            .findOneWithAuthoritiesByEmailIgnoreCase(login).map(user -> createSpringSecurityUser(login, user));
+            return userPersistencPort
+              .findOneWithAuthoritiesByEmailIgnoreCase(login).map(user -> createSpringSecurityUser(login, user)).
+              orElseThrow(() -> new UsernameNotFoundException("User with email " + login + " was not found in the database"));
+       
         }
 
         String lowercaseLogin = login.toLowerCase(Locale.ENGLISH);
